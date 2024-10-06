ed81e3b75d26b6d5f1fe7108a870dd46da20861e
Ramon Brullo <kingvanti650@gmail.com>
2024-10-06 17:41:49 +0200

    fix: memory issue

--- Assets/Code/Common/Singletons.cs
+++ Assets/Code/Common/Singletons.cs
@@ -1,7 +1,6 @@
 #nullable enable
 
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace GTC
@@ -10,23 +9,17 @@ namespace GTC
     {
         private static GameObject? host;
 
-        private static GameObject Host
+        private static GameObject? Host
         {
             get
             {
                 if (host == null || !host)
                     host = GameObject.FindWithTag("Singletons");
-                if (host == null)
-                    throw new NullReferenceException(
-                        "Singletons could not be found");
                 return host;
             }
         }
 
-        public static IEnumerable<MonoBehaviour> All() =>
-            Host.GetComponents<MonoBehaviour>();
-
-        public static T? TryGet<T>() where T : class => Host.GetComponent<T>();
+        public static T? TryGet<T>() where T : class => Host?.GetComponent<T>();
 
         public static T Get<T>() where T : class =>
             TryGet<T>() ??
