54dac50287918ce47d6a719dd2df468091966717
Ramon Brullo <kingvanti650@gmail.com>
2024-10-06 17:41:42 +0200

    feat: start pedestal

--- Assets/Code/Level/LevelBuilder.cs
+++ Assets/Code/Level/LevelBuilder.cs
@@ -6,7 +6,9 @@ namespace GTC.Level
 {
     public class LevelBuilder : MonoBehaviour
     {
+        [SerializeField] private float fleaOffset;
         [SerializeField] private GameObject fleaPrefab;
+        [SerializeField] private GameObject startPrefab;
         [SerializeField] private GameObject targetPrefab;
 
         public async Task<Level> Build(LevelData levelData,
@@ -15,6 +17,9 @@ namespace GTC.Level
             var root = new GameObject("Level");
 
             var flea = Instantiate(fleaPrefab, levelData.FleaPosition,
+                Quaternion.identity, root.transform);
+            _ = Instantiate(startPrefab,
+                levelData.FleaPosition + Vector2.down * fleaOffset,
                 Quaternion.identity, root.transform);
 
             await Task.Yield();
